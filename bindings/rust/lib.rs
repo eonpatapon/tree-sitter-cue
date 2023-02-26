67500cd14056c3b375174e66ed4c535c390fdf2b
Amaan Qureshi <amaanq12@gmail.com>
2023-02-26 06:55:31 -0500

    feat: update bindings

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,4 +1,9 @@
-//! This crate provides cue language support for the [tree-sitter][] parsing library.
+// ------------------------------------------------------------------------------------------------
+// Copyright © 2023, Jean-Philippe Braun <eon@patapon.info>, Amaan Qureshi <amaanq12@gmail.com>
+// See the LICENSE file in this repo for license details.
+// ------------------------------------------------------------------------------------------------
+
+//! This crate provides Cue language support for the [tree-sitter][] parsing library.
 //!
 //! Typically, you will use the [language][language func] function to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
@@ -6,7 +11,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_cue::language()).expect("Error loading cue grammar");
+//! parser.set_language(tree_sitter_cue::language()).expect("Error loading Cue grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -28,17 +33,28 @@ pub fn language() -> Language {
     unsafe { tree_sitter_cue() }
 }
 
+/// The source of the Rust tree-sitter grammar description.
+pub const GRAMMAR: &str = include_str!("../../grammar.js");
+
+/// The folds query for this language.
+pub const FOLDS_QUERY: &str = include_str!("../../queries/folds.scm");
+
+/// The syntax highlighting query for this language.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+
+/// The indents query for this language.
+pub const INDENTS_QUERY: &str = include_str!("../../queries/indents.scm");
+
+/// The injection query for this language.
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+
+/// The symbol tagging query for this language.
+pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
-
-// Uncomment these to include any queries that this grammar contains
-
-// pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
-// pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-// pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
@@ -47,6 +63,6 @@ mod tests {
         let mut parser = tree_sitter::Parser::new();
         parser
             .set_language(super::language())
-            .expect("Error loading cue language");
+            .expect("Error loading Cue grammar");
     }
 }
